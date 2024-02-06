af8aa09bfa65d9fd6260b109b73380283873a7f6
OmerAhmedSiddiqui786 <omerahmedsiddiqui337@gmail.com>
2024-02-06 15:07:34 +0500

    passwords change

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'cqdkfcyyew' );
+define( 'DB_NAME', 'cosmetics' );
 
 /** Database username */
-define( 'DB_USER', 'cqdkfcyyew' );
+define( 'DB_USER', 'root' );
 
 /** Database password */
 define( 'DB_PASSWORD', 'root' );
 
 /** Database hostname */
-define( 'DB_HOST', 'anKHrh2hPf' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
